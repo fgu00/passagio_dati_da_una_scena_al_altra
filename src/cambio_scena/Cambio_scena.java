420870e4c6523dfc2167689257dae5417c43ee6d
russo.salvatore <russo.salvatore@TAVE-PC27.OSDBSESTO.local>
2021-05-10 08:53:51 +0200

    add

---
+++ src/cambio_scena/Cambio_scena.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cambio_scena;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author russo.salvatore
+ */
+public class Cambio_scena extends Application {
+    private guiScena1 scena1;
+    private guiScena2 scena2;
+    @Override
+    public void start(Stage primaryStage) {
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+         scena1=new guiScena1(primaryStage);
+         scena2=new guiScena2(primaryStage); 
+         scena1.setScena2(scena2.getScena());
+         scena2.setScena2(scena1.getScena());
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        
+//        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scena1.getScena());
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/cambio_scena/guiScena1.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cambio_scena;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author russo.salvatore
+ */
+public class guiScena1 {
+    private Stage s;
+    private Scene scena;
+    private Scene scena2;
+    private HBox h;
+     private  Label l;
+      private Button btn ;
+    public guiScena1(Stage s){
+     this.s=s;
+     gui();
+    }
+    public void gui(){
+         h=new HBox();
+         l=new Label("scena 1");
+           btn = new Button();
+        btn.setText("va alla scena due");
+        btn.setOnMouseClicked((value->{
+            //imposto la nuova scena
+           s.setScene(scena2);
+           //mostro la scena imposta
+           s.show();
+        }));
+          h.getChildren().addAll(l,btn);
+        scena =new Scene(h, 300, 250);  
+    }
+    public Scene getScena(){
+        return scena;    
+    }
+    public void setScena2(Scene scena2){
+        this.scena2=scena2;
+    }
+}
